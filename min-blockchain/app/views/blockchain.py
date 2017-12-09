495f77b735ecb066116a517bd8cfb5835b4d0349
PlanB <city7310@naver.com>
2017-12-09 16:32:11 +0900

    [DONE] Mine

---
+++ min-blockchain/app/views/blockchain.py
@@ -0,0 +1,33 @@
+from flask_restful import Resource, request
+
+from app import node_id
+from blockchain.blockchain import Blockchain
+
+blockchain = Blockchain()
+
+
+class Mine(Resource):
+    def get(self):
+        last_block = blockchain.last_block
+        nonce = blockchain.proof_of_work(last_block['nonce'])
+        # Mine
+
+        blockchain.new_transaction(
+            sender='0',
+            recipient=node_id,
+            amount=1
+        )
+
+        previous_hash = blockchain.hash_block(last_block)
+        new_block = blockchain.new_block(nonce, previous_hash)
+        # Generates new block
+
+        return {
+            'message': 'New Block Forged',
+            'block': {
+                'version': new_block['version'],
+                'transactions': new_block['transactions'],
+                'timestamp': new_block['timestamp'],
+                'nonce': new_block['nonce']
+            }
+        }, 200
