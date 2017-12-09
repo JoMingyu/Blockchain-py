f612fe3c20d7c4690c07a6f2b945b1ef64ad2bb9
PlanB <city7310@naver.com>
2017-12-09 17:41:01 +0900

    [DONE] Node

--- min-blockchain/app/views/blockchain.py
+++ min-blockchain/app/views/blockchain.py
@@ -1,9 +1,33 @@
+from flask import Response
 from flask_restful import Resource, request
 
 from app import node_id
 from blockchain.blockchain import Blockchain
 
 blockchain = Blockchain()
+
+
+class Node(Resource):
+    def post(self):
+        if not request.is_json:
+            return Response('', 400)
+
+        req = request.get_json()
+        nodes = req.get('nodes')
+
+        if not all([nodes]):
+            return Response('', 400)
+
+        if isinstance(nodes, list):
+            return Response('Nodes must be a list', 400)
+
+        for node in nodes:
+            blockchain.register_node(node)
+
+        return {
+            'message': 'New nodes have been added.',
+            'nodes': list(blockchain.nodes)
+        }, 201
 
 
 class Chain(Resource):
@@ -13,7 +37,7 @@ class Chain(Resource):
         return {
             'chains': chains,
             'length': len(chains)
-        }
+        }, 200
 
 
 class Mine(Resource):
@@ -45,8 +69,15 @@ class Mine(Resource):
 
 class Transaction(Resource):
     def post(self):
-        sender = request.form['sender']
-        recipient = request.form['recipient']
-        amount = request.form['amount']
+        if not request.is_json:
+            return Response('', 400)
+
+        req = request.get_json()
+        sender = req.get('sender')
+        recipient = req.get('recipient')
+        amount = req.get('amount')
+
+        if not all([sender, recipient, amount]):
+            return Response('', 400)
 
         blockchain.new_transaction(sender, recipient, amount)
